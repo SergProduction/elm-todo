a97f5ea7841f38eb0fdad16c48a9c333a0a54427
Sergey Filippov <sdweb@ro.ru>
2019-02-07 13:32:51 +0300

    run dev with elm-live

--- src/Main.elm
+++ src/Main.elm
@@ -1,104 +1,147 @@
+module Main exposing (EditTask(..), Model, Msg(..), TreeTask(..), init, main, recViewTaskTree, subscriptions, update, view, viewTaskEditable)
+
 import Browser
-import Html exposing (Html, node, text, div, button, ul, li, span, input, textarea)
-import Html.Events exposing (onClick, onInput)
-import Html.Attributes exposing (rel, href, class, type_, value, placeholder)
 import Debug
-import TaskStruct as T exposing (
-  Task(..)
-  , createTask
-  , mapTask, filterTask
-  , addTask, removeTask
-  , findAndUpdateTaskName, findAndUpdateTaskDesk
-  )
+import Html exposing (Html, button, div, input, li, node, span, text, textarea, ul)
+import Html.Attributes exposing (class, href, placeholder, rel, type_, value)
+import Html.Events exposing (onClick, onInput)
+import TaskStruct as T
+    exposing
+        ( Task(..)
+        , addTask
+        , createTask
+        , filterTask
+        , findAndUpdateTaskDesk
+        , findAndUpdateTaskName
+        , mapTask
+        , removeTask
+        )
 
 
-
-main = Browser.sandbox
-  { init = init
-  , update = update
-  , view = view }
+main =
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
 
 
 type alias Model =
-  { taskTree: Task
-  , taskEditable: Maybe Task
-  }
+    { taskTree : Task
+    , taskEditable : Maybe Task
+    }
 
 
-init : Model
-init =
-  { taskTree = createTask "begin"
-  , taskEditable = Nothing
-  }
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { taskTree = createTask "begin"
+      , taskEditable = Nothing
+      }
+    , Cmd.none
+    )
+
 
 type TreeTask
-  = Add Int Int
-  | Del Int Int
+    = Add Int Int
+    | Del Int Int
+
 
 type EditTask
-  = Edit Task
-  | Name String
-  | Desk String
+    = Edit Task
+    | Name String
+    | Desk String
 
 
 type Msg
-  = TreeTask TreeTask
-  | EditTask EditTask
+    = TreeTask TreeTask
+    | EditTask EditTask
 
-update : Msg -> Model -> Model
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    TreeTask (Add pos lvl) -> { model | taskTree = addTask pos lvl model.taskTree }
-    TreeTask (Del pos lvl) -> case removeTask pos lvl model.taskTree of
-      Nothing -> { model | taskTree =  createTask "main task" }
-      Just t  -> { model | taskTree = t }
-    EditTask (Edit t) -> {model | taskEditable = Just t }
-    EditTask (Name taskName) -> case model.taskEditable of
-      Nothing       -> { model | taskEditable = Just <| createTask taskName } -- такого не может быть. но почему-то мне нужно обработать этот случай
-      Just (Task t) ->
-        { model
-        | taskEditable = Just <| Task { t | name = taskName }
-        , taskTree = findAndUpdateTaskName model.taskTree (Task t) taskName
-        }
-    EditTask (Desk desk) -> case model.taskEditable of
-      Nothing       -> { model | taskEditable = Just <| createTask "" } -- такого не может быть. но почему-то мне нужно обработать этот случай
-      Just (Task t) ->
-        { model
-        | taskEditable = Just <| Task { t | desk = desk }
-        , taskTree = findAndUpdateTaskDesk model.taskTree (Task t) desk
-        }
+    case msg of
+        TreeTask (Add pos lvl) ->
+            ( { model | taskTree = addTask pos lvl model.taskTree }, Cmd.none )
 
+        TreeTask (Del pos lvl) ->
+            case removeTask pos lvl model.taskTree of
+                Nothing ->
+                    ( { model | taskTree = createTask "main task" }, Cmd.none )
+
+                Just t ->
+                    ( { model | taskTree = t }, Cmd.none )
+
+        EditTask (Edit t) ->
+            ( { model | taskEditable = Just t }, Cmd.none )
+
+        EditTask (Name taskName) ->
+            case model.taskEditable of
+                Nothing ->
+                    ( { model | taskEditable = Just <| createTask taskName }, Cmd.none )
+
+                -- такого не может быть. но почему-то мне нужно обработать этот случай
+                Just (Task t) ->
+                    ( { model
+                        | taskEditable = Just <| Task { t | name = taskName }
+                        , taskTree = findAndUpdateTaskName model.taskTree (Task t) taskName
+                      }
+                    , Cmd.none
+                    )
+
+        EditTask (Desk desk) ->
+            case model.taskEditable of
+                Nothing ->
+                    ( { model | taskEditable = Just <| createTask "" }, Cmd.none )
+
+                -- такого не может быть. но почему-то мне нужно обработать этот случай
+                Just (Task t) ->
+                    ( { model
+                        | taskEditable = Just <| Task { t | desk = desk }
+                        , taskTree = findAndUpdateTaskDesk model.taskTree (Task t) desk
+                      }
+                    , Cmd.none
+                    )
 
 
 view : Model -> Html Msg
 view model =
-  div [ class "main" ]
-    [ node "link" [ rel "stylesheet", href "main.css" ] []
-    , div [class "task-tree"] [recViewTaskTree model.taskTree]
-    , div [] [viewTaskEditable model.taskEditable]
-    ]
+    div [ class "main" ]
+        [ node "link" [ rel "stylesheet", href "main.css" ] []
+        , div [ class "task-tree" ] [ recViewTaskTree model.taskTree ]
+        , div [] [ viewTaskEditable model.taskEditable ]
+        ]
 
 
 recViewTaskTree : Task -> Html Msg
 recViewTaskTree (Task task) =
-  ul []
-    [ li [class "name" ]
-      [ div [class "del", onClick (TreeTask <| Del task.pos task.lvl)] [text "del"]
-      , div [class "add", onClick (TreeTask <| Add task.pos task.lvl)] [text "add"]
-      , div [class "id"]
-        [ span [] [text (String.fromInt task.lvl ++ ".")]
-        , span [] [text (String.fromInt task.pos)]
+    ul []
+        [ li [ class "name" ]
+            [ div [ class "del", onClick (TreeTask <| Del task.pos task.lvl) ] [ text "del" ]
+            , div [ class "add", onClick (TreeTask <| Add task.pos task.lvl) ] [ text "add" ]
+            , div [ class "id" ]
+                [ span [] [ text (String.fromInt task.lvl ++ ".") ]
+                , span [] [ text (String.fromInt task.pos) ]
+                ]
+            , div [ onClick <| EditTask <| Edit (Task task) ] [ text task.name ]
+            ]
+        , li [] (List.map recViewTaskTree task.children)
         ]
-      , div [ onClick <| EditTask <| Edit (Task task) ] [text task.name]
-      ]
-    , li [] (List.map recViewTaskTree task.children)
-    ]
 
 
 viewTaskEditable : Maybe Task -> Html Msg
-viewTaskEditable t = case t of
-  Nothing -> text ""
-  Just (Task task) -> div [class "editable"]
-    [ input [type_ "text", value task.name, onInput (\n -> EditTask (Name n))] []
-    , textarea [onInput (\n -> EditTask (Desk n)), value task.desk] [text task.desk]
-    ]
+viewTaskEditable t =
+    case t of
+        Nothing ->
+            text ""
+
+        Just (Task task) ->
+            div [ class "editable" ]
+                [ input [ type_ "text", value task.name, onInput (\n -> EditTask (Name n)) ] []
+                , textarea [ onInput (\n -> EditTask (Desk n)), value task.desk ] [ text task.desk ]
+                ]
--- src/TaskStruct.elm
+++ src/TaskStruct.elm
@@ -1,103 +1,144 @@
-module TaskStruct exposing (
-  Task(..)
-  , createTask
-  , mapTask, filterTask
-  , addTask, removeTask
-  , findAndUpdateTaskName, findAndUpdateTaskDesk
-  )
+module TaskStruct exposing
+    ( Task(..)
+    , addTask
+    , createTask
+    , filterTask
+    , findAndUpdateTaskDesk
+    , findAndUpdateTaskName
+    , mapTask
+    , removeTask
+    )
+
+import Json.Decode as D
+import Json.Encode as E
 import Maybe.Extra exposing (values)
 
-type Status
-  = Planned
-  | Complete
-  | Error
+
 
 {-
-type alias Branch =
-  { name : String
-  , text : String
-  , children : List Task
-  }
+   type Status
+       = Planned
+       | Complete
+       | Error
 -}
 
-type Task =
-    Task
-    { name : String
-    , status: Status
-    , desk: String
-    , pos : Int
-    , lvl : Int
-    -- , branches : Dict String, Branch
-    , children : List Task
-    }
+
+type Task
+    = Task
+        { name : String
+        , status : String
+        , desk : String
+        , pos : Int
+        , lvl : Int
+
+        -- , branches : Dict String, Branch
+        , children : List Task
+        }
 
 
-mapTask : (Task -> Task) -> Task -> Task 
+
+{-
+   decoder : D.Decoder Task
+   decoder =
+       let d = D.map6 Tsk
+           (D.field "name" D.string)
+           (D.field "status" D.string)
+           (D.field "desk" D.string)
+           (D.field "pos" D.int)
+           (D.field "lvl" D.int)
+           (D.field "children" D.list (D.lazy (\_ -> decoder)))
+       in
+         case d of
+           Ok dd -> Task dd
+           Err _ -> Err _
+-}
+
+
+mapTask : (Task -> Task) -> Task -> Task
 mapTask f t =
-  let
-    (Task task) = f t
-  in
+    let
+        (Task task) =
+            f t
+    in
     Task { task | children = List.map (mapTask f) task.children }
 
 
-filterTask : (Task -> Bool) -> Task -> Maybe Task 
+filterTask : (Task -> Bool) -> Task -> Maybe Task
 filterTask f (Task t) =
-  if f (Task t) then
-    Just ( Task { t | children = t.children |> List.map (filterTask f) |> values } )
-  else
-    Nothing
+    if f (Task t) then
+        Just (Task { t | children = t.children |> List.map (filterTask f) |> values })
+
+    else
+        Nothing
 
 
 addTaskByPosAndLvl : Int -> Int -> Task -> Task
 addTaskByPosAndLvl pos lvl (Task task) =
-  if task.pos == pos && task.lvl == lvl
-    then
-      Task { task |
-             children = List.append
-               task.children
-               [ createTaskPosLvlName (List.length task.children) (lvl + 1) "new task" ]
-      }
+    if task.pos == pos && task.lvl == lvl then
+        Task
+            { task
+                | children =
+                    List.append
+                        task.children
+                        [ createTaskPosLvlName (List.length task.children) (lvl + 1) "new task" ]
+            }
+
     else
-      Task task
+        Task task
 
 
 addTask : Int -> Int -> Task -> Task
-addTask pos lvl t = mapTask (addTaskByPosAndLvl pos lvl) t
+addTask pos lvl t =
+    mapTask (addTaskByPosAndLvl pos lvl) t
+
 
 removeTask : Int -> Int -> Task -> Maybe Task
-removeTask pos lvl task = filterTask
-  ( \(Task t) -> not (t.pos == pos && t.lvl == lvl) )
-  task
+removeTask pos lvl task =
+    filterTask
+        (\(Task t) -> not (t.pos == pos && t.lvl == lvl))
+        task
+
 
 createTaskPosLvlName : Int -> Int -> String -> Task
-createTaskPosLvlName pos lvl name = Task
-  { name = name
-  , status = Planned
-  , desk = ""
-  , pos = pos
-  , lvl = lvl
-  -- , branches = empty
-  , children = []
-  }
+createTaskPosLvlName pos lvl name =
+    Task
+        { name = name
+        , status = "Planned"
+        , desk = ""
+        , pos = pos
+        , lvl = lvl
+
+        -- , branches = empty
+        , children = []
+        }
+
 
 createTask : String -> Task
-createTask = createTaskPosLvlName 0 0
+createTask =
+    createTaskPosLvlName 0 0
 
 
 findAndUpdateTaskName : Task -> Task -> String -> Task
 findAndUpdateTaskName treeTask (Task oneTask) taskName =
-  mapTask (\(Task t) -> if t.lvl == oneTask.lvl && t.pos == oneTask.pos
-      then
-        Task { t | name = taskName }
-      else
-        (Task t)
-  ) treeTask
+    mapTask
+        (\(Task t) ->
+            if t.lvl == oneTask.lvl && t.pos == oneTask.pos then
+                Task { t | name = taskName }
+
+            else
+                Task t
+        )
+        treeTask
+
 
 findAndUpdateTaskDesk : Task -> Task -> String -> Task
 findAndUpdateTaskDesk treeTask (Task oneTask) desk =
-  mapTask (\(Task t) -> if t.lvl == oneTask.lvl && t.pos == oneTask.pos
-      then
-        Task { t | desk = desk }
-      else
-        (Task t)
-  ) treeTask
+    mapTask
+        (\(Task t) ->
+            if t.lvl == oneTask.lvl && t.pos == oneTask.pos then
+                Task { t | desk = desk }
+
+            else
+                Task t
+        )
+        treeTask
